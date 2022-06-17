a4f87772e5725fca5449f38c3d6fc2d2300008fc
Kadi <gueneykadircan@gmail.com>
2022-06-17 12:15:57 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+console.log("text");
+
+//lev1_2: getElementsByClassName
+
+// let example = document.getElementsByClassName('example');
+
+// console.log(example);
+
+// myFunction = () => {
+//     for (let i = 0; i < example.length; i++) {
+//         console.log(example[i].style.backgroundColor = "pink");
+//     }
+// }
+// console.log(example.length)
+
+// ======= LEVEL 2-1 ===============
+let home = document.getElementById("navHome");
+
+function myFunction() {
+    home.style.backgroundColor = "tomato";
+    home.innerHTML = "emoH"
+
+    console.log("test");
+}
+
+
+
+
+
